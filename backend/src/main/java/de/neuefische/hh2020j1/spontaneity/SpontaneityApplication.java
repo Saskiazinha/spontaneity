209c7807927b56ae65a756e6588ed7334d4d1c58
Saskia <busch.saskia09@gmail.com>
2020-11-16 10:41:34 +0100

    Init spring server

---
+++ backend/src/main/java/de/neuefische/hh2020j1/spontaneity/SpontaneityApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.hh2020j1.spontaneity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpontaneityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpontaneityApplication.class, args);
+	}
+
+}
