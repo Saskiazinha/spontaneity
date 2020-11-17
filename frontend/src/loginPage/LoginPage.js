6561e58a19af04b1fb8e13e102030d911c303cea
Saskia <busch.saskia09@gmail.com>
2020-11-17 18:56:18 +0100

    Frontend - add Form on LoginPage

--- frontend/src/loginPage/LoginPage.js
+++ frontend/src/loginPage/LoginPage.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
-  return <div>Login</div>;
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  return (
+    <>
+      {/*Placeholder Header Component*/}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            name="username"
+            value={loginData.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+            type="password"
+          />
+        </label>
+        <button>Login</button>
+      </form>
+    </>
+  );
+  function handleChange(event) {
+    setLoginData({ ...loginData, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 }
