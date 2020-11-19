401a1595f2f449462bb45b5dfe73998a92a60fae
Saskia <busch.saskia09@gmail.com>
2020-11-19 07:58:36 +0100

    Add post model without timeframe

--- backend/src/main/java/de/neuefische/hh2020j1/spontaneity/model/Post.java
+++ backend/src/main/java/de/neuefische/hh2020j1/spontaneity/model/Post.java
@@ -1,11 +1,25 @@
 package de.neuefische.hh2020j1.spontaneity.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "post")
 public class Post {
-
+    @Id
     private String id;
     private String creator;
+    private String location;
+    private String category;
+    private String notes;
+    private Instant timestamp;
+
 
 }
