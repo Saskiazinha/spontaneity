934809abb61fcd4d83ace357f94a0baa88e9ff11
Saskia <busch.saskia09@gmail.com>
2020-11-19 14:32:06 +0100

    init signUpPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import LoginPage from "./loginPage/LoginPage";
+import LoginPage from "./security/LoginPage";
 import PostsToday from "./postsPages/PostsToday";
 import UserContextProvider from "./contexts/UserContextProvider";
 import ProtectedRoute from "./routing/ProtectedRoute";
+import SignUpPage from "./security/SignUpPage";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Switch>
         <Route path="/login">
           <LoginPage />
+        </Route>
+        <Route path="signup">
+          <SignUpPage />
         </Route>
         <ProtectedRoute path={["/", "/home"]}>
           <PostsToday />
--- frontend/src/security/LoginPage.js
+++ frontend/src/security/LoginPage.js
@@ -8,30 +8,27 @@ export default function LoginPage() {
   const [error, setError] = useState("");
   const history = useHistory();
   return (
-    <>
-      {/*Placeholder Header Component*/}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username
-          <input
-            name="username"
-            value={loginData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            name="password"
-            value={loginData.password}
-            onChange={handleChange}
-            type="password"
-          />
-        </label>
-        {error ?? <p>{error}</p>}
-        <button>Login</button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username
+        <input
+          name="username"
+          value={loginData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          name="password"
+          value={loginData.password}
+          onChange={handleChange}
+          type="password"
+        />
+      </label>
+      <button>Login</button>
+      {error ?? <p>{error}</p>}
+    </form>
   );
   function handleChange(event) {
     setLoginData({ ...loginData, [event.target.name]: event.target.value });
