ae5eea18eb8cadb7453f98f79e8bdab06cb68a4e
Saskia <busch.saskia09@gmail.com>
2020-11-20 16:56:01 +0100

    add query to sort posts chronologically

--- backend/src/main/java/de/neuefische/hh2020j1/spontaneity/service/PostService.java
+++ backend/src/main/java/de/neuefische/hh2020j1/spontaneity/service/PostService.java
@@ -3,6 +3,9 @@ package de.neuefische.hh2020j1.spontaneity.service;
 import de.neuefische.hh2020j1.spontaneity.dao.PostDao;
 import de.neuefische.hh2020j1.spontaneity.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +13,19 @@ import java.util.List;
 @Service
 public class PostService {
     private final PostDao postDao;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public PostService(PostDao postDao) {
+    public PostService(PostDao postDao, MongoTemplate mongoTemplate) {
         this.postDao = postDao;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public List<Post> getIdeas() {
-        return postDao.findAll();
+        Query querySortByTime = new Query();
+        querySortByTime.with(Sort.by(Sort.Direction.ASC,"startPoint"));
+
+        List<Post> posts = mongoTemplate.find(querySortByTime, Post.class);
+        return posts;
     }
 }
