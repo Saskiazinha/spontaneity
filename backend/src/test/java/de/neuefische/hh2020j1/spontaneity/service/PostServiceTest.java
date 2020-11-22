0f80f70f78648395f0027f2fc1070d26eaf717e2
Saskia <busch.saskia09@gmail.com>
2020-11-22 01:48:47 +0100

    Fix PostServiceTest

--- backend/src/test/java/de/neuefische/hh2020j1/spontaneity/service/PostServiceTest.java
+++ backend/src/test/java/de/neuefische/hh2020j1/spontaneity/service/PostServiceTest.java
@@ -6,6 +6,7 @@ import de.neuefische.hh2020j1.spontaneity.model.Post;
 import de.neuefische.hh2020j1.spontaneity.seeder.PostSeeder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,20 +18,21 @@ import static org.mockito.Mockito.*;
 class PostServiceTest {
 
     private final PostDao postDao=mock(PostDao.class);
-    private final MongoTemplate mongoTemplate=mock(MongoTemplate.class);
+    MongoTemplate mongoTemplate=mock(MongoTemplate.class);
     private final PostService postService=new PostService(postDao, mongoTemplate);
 
-//    @Test
-//    @DisplayName("The \"getIdeasSortedByTime\" method should return posts in the order of their startPoint")
-//    void getIdeasSortedByTimeTest(){
-//        //Given
-//        Query querySortByTime = new Query();
-//        when(mongoTemplate.find(querySortByTime, Post.class)).thenReturn(PostSeeder.getStockPostsSorted());
-//
-//        //When
-//        List<SendPostDto> allPosts= postService.getIdeasSortedByTime();
-//
-//        //Then
-//        assertThat(allPosts,is(PostSeeder.getStockSendPostsDtoSorted()));
-//    }
+    @Test
+    @DisplayName("The \"getIdeasSortedByTime\" method should return posts in the order of their startPoint")
+    void getIdeasSortedByTimeTest(){
+        //Given
+        Query querySortByTime = new Query();
+        querySortByTime.with(Sort.by(Sort.Direction.ASC,"startPoint"));
+        when(mongoTemplate.find(querySortByTime, Post.class)).thenReturn(PostSeeder.getStockPostsSorted());
+
+        //When
+        List<SendPostDto> allPosts= postService.getIdeasSortedByTime();
+
+        //Then
+        assertThat(allPosts,is(PostSeeder.getStockSendPostsDtoSorted()));
+    }
 }
