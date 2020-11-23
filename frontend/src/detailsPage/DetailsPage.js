a846bc6112498cf38abfd3e724406489ff94c457
Saskia <busch.saskia09@gmail.com>
2020-11-23 19:06:01 +0100

    Update DetailsPage.js content

--- frontend/src/detailsPage/DetailsPage.js
+++ frontend/src/detailsPage/DetailsPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import PostContext from "../contexts/PostContext";
+import SpontaneityHeader from "../commons/SpontaneityHeader";
 
 export default function DetailsPage() {
   const { posts } = useContext(PostContext);
@@ -8,7 +9,17 @@ export default function DetailsPage() {
   const post = posts.find((post) => post.id === id);
   return (
     <div>
-      <div>post.creator</div>
+      <SpontaneityHeader />
+      <h2>Details</h2>
+      <p>{post.creator}</p>
+      <p>
+        Time: {post.startPoint} - {post.endPoint}
+      </p>
+      <p>Preferred Location: {post.location}</p>
+      <p>{post.statusLocation}</p>
+      <p>Category: {post.category}</p>
+      <p>Notes: {post.notes}</p>
+      <p>{post.timestamp}</p>
     </div>
   );
 }
