735593911d5ee67643ce5df31ef08f9e519dc963
Saskia <busch.saskia09@gmail.com>
2020-11-23 19:39:16 +0100

    Content

--- frontend/src/detailsPage/DetailsPage.js
+++ frontend/src/detailsPage/DetailsPage.js
@@ -13,11 +13,10 @@ export default function DetailsPage() {
       <h2>Details</h2>
       <p>{post.creator}</p>
       <p>
-        Time: {post.startPoint} - {post.endPoint}
+        Icon {post.startPoint} - {post.endPoint}
       </p>
-      <p>Preferred Location: {post.location}</p>
-      <p>{post.statusLocation}</p>
-      <p>Category: {post.category}</p>
+      <p>Icon {post.location}</p>
+      <p>Icon {post.category}</p>
       <p>Notes: {post.notes}</p>
       <p>{post.timestamp}</p>
     </div>
