8d795a32d1356279ac63e6c3247c2609f726432f
Saskia <busch.saskia09@gmail.com>
2020-11-25 19:23:28 +0100

    add mypost pages

---
+++ frontend/src/myPostsPages/MyPostsDayAfterTomorrow.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import PostContext from "../contexts/PostContext";
+import { getDate } from "../utils/DateUtils";
+import SpontaneityHeader from "../commons/SpontaneityHeader";
+import NavigationHeader from "../commons/NavigationHeader";
+import PostList from "../postsCommons/PostList";
+import Footer from "../commons/Footer";
+
+export default function MyPostsDayAfterTomorrow() {
+  const { myPosts } = useContext(PostContext);
+  const myPostsFiltered = myPosts.filter(
+    (myPost) => myPost.localDate === getDate(2)
+  );
+  return (
+    <>
+      <SpontaneityHeader />
+      <NavigationHeader
+        title={"My Posts"}
+        day={"Day After Tomorrow"}
+        postType={"myposts"}
+      />
+      <PostList posts={myPostsFiltered} />
+      <Footer />
+    </>
+  );
+}
---
+++ frontend/src/myPostsPages/MyPostsTomorrow.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import PostContext from "../contexts/PostContext";
+import { getDate } from "../utils/DateUtils";
+import SpontaneityHeader from "../commons/SpontaneityHeader";
+import NavigationHeader from "../commons/NavigationHeader";
+import PostList from "../postsCommons/PostList";
+import Footer from "../commons/Footer";
+
+export default function MyPostsToday() {
+  const { myPosts } = useContext(PostContext);
+  const myPostsFiltered = myPosts.filter(
+    (myPost) => myPost.localDate === getDate(1)
+  );
+  return (
+    <>
+      <SpontaneityHeader />
+      <NavigationHeader
+        title={"My Posts"}
+        day={"Tomorrow"}
+        postType={"myposts"}
+      />
+      <PostList posts={myPostsFiltered} />
+      <Footer />
+    </>
+  );
+}
