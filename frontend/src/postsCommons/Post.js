8699ace7e4514d9fba6c71b97a17f73e89e1e73a
Saskia <busch.saskia09@gmail.com>
2020-11-25 13:16:20 +0100

    small grid updates

--- frontend/src/postsCommons/Post.js
+++ frontend/src/postsCommons/Post.js
@@ -95,7 +95,7 @@ const PostStyled = styled.div`
 
   button {
     grid-row: 1;
-    grid-column: 3 / 3;
+    grid-column: 3;
 
     justify-self: end;
   }
@@ -103,7 +103,7 @@ const PostStyled = styled.div`
 
 const NameStyling = styled.h4`
   grid-row: 1;
-  grid-column: 2 / 2;
+  grid-column: 2;
   align-self: center;
   justify-self: center;
   font-size: 1.1em;
@@ -113,7 +113,7 @@ const NameStyling = styled.h4`
 `;
 
 const ContentStyling = styled.div`
-  grid-row: 2 / 2;
+  grid-row: 2;
   grid-column: span 3;
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
