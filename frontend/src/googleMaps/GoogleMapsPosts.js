de1b518be8e016ca89b952e84b0fcbe936cc891d
Saskia <busch.saskia09@gmail.com>
2020-12-02 13:24:18 +0100

    Init maps

--- frontend/src/googleMaps/GoogleMapsPosts.js
+++ frontend/src/googleMaps/GoogleMapsPosts.js
@@ -1,5 +1,35 @@
 import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+// import usePlacesAutocomplete, {
+//   getGeocode,
+//   getLatLng,
+// } from "use-places-autocomplete";
+// import {
+//   Combobox,
+//   ComboboxInput,
+//   ComboboxPopover,
+//   ComboboxList,
+//   ComboboxOption,
+// } from "@reach/combobox";
+import { formatRelative } from "date-fns";
+import "@reach/combobox/styles.css";
+// import mapStyles from "./mapStyles";
+
+const libraries = ["places"];
 
 export default function GoogleMapsPosts() {
-  return <div>Placeholder</div>;
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return <div>map</div>;
 }
