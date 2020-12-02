b5a02f3ca9854545a190933bfaa519f28cec97ea
Saskia <busch.saskia09@gmail.com>
2020-12-02 17:55:21 +0100

    update seeder

--- backend/src/main/java/de/neuefische/hh2020j1/spontaneity/model/Location.java
+++ backend/src/main/java/de/neuefische/hh2020j1/spontaneity/model/Location.java
@@ -1,13 +1,16 @@
 package de.neuefische.hh2020j1.spontaneity.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Location {
     private String lat;
     private String lng;
+    private String district;
 }
--- backend/src/test/java/de/neuefische/hh2020j1/spontaneity/seeder/PostSeeder.java
+++ backend/src/test/java/de/neuefische/hh2020j1/spontaneity/seeder/PostSeeder.java
@@ -1,6 +1,7 @@
 package de.neuefische.hh2020j1.spontaneity.seeder;
 
 import de.neuefische.hh2020j1.spontaneity.dto.SendPostDto;
+import de.neuefische.hh2020j1.spontaneity.model.Location;
 import de.neuefische.hh2020j1.spontaneity.model.Post;
 import de.neuefische.hh2020j1.spontaneity.model.EnumCategory;
 import de.neuefische.hh2020j1.spontaneity.model.EnumStatus;
@@ -15,74 +16,74 @@ public class PostSeeder {
 
     public static final List<Post> getStockPostsSorted(){
         return new ArrayList<>(List.of(
-                new Post("111", "Franzi","Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.BLUE, EnumCategory.Drinks, EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,"Barmbek" , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,"Eimsbüttel" , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
+                new Post("111", "Franzi","Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build(), EnumStatus.BLUE, EnumCategory.Drinks, EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,Location.builder().district("Altona").build(), EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Barmbek").build(), EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Eimsbüttel").build() , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
         ));
     }
 
     public static final List<Post> getStockPostsUnsorted(){
         return new ArrayList<>(List.of(
-                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,"Barmbek" , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,"Eimsbüttel" , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("111","Franzi", "Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
+                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Barmbek").build() , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Eimsbüttel").build() , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("111","Franzi", "Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
 
         ));
     }
 
     public static final List<SendPostDto> getStockSendPostsDtoSorted(){
         return new ArrayList<SendPostDto>(List.of(
-                new SendPostDto("111", "Franzi","Drink out", LocalDate.of(2020,11,20), LocalTime.of(10,00), LocalTime.of(11,00),EnumStatus.GREEN,"Sternschanze" , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("333", "Janosch","Catching dinner", LocalDate.of(2020,11,21),LocalTime.of(13,00), LocalTime.of(14,00),EnumStatus.GREEN,"Barmbek" , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("444", "Carsten","Music", LocalDate.of(2020,11,22),LocalTime.of(16,00), LocalTime.of(18,00),EnumStatus.GREEN,"Sternschanze" , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("555", "Franzi","Drink out", LocalDate.of(2020,11,22),LocalTime.of(21,00), LocalTime.of(23,00),EnumStatus.GREEN,"Eimsbüttel" , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
+                new SendPostDto("111", "Franzi","Drink out", LocalDate.of(2020,11,20), LocalTime.of(10,00), LocalTime.of(11,00),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("333", "Janosch","Catching dinner", LocalDate.of(2020,11,21),LocalTime.of(13,00), LocalTime.of(14,00),EnumStatus.GREEN,Location.builder().district("Barmbek").build() , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("444", "Carsten","Music", LocalDate.of(2020,11,22),LocalTime.of(16,00), LocalTime.of(18,00),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("555", "Franzi","Drink out", LocalDate.of(2020,11,22),LocalTime.of(21,00), LocalTime.of(23,00),EnumStatus.GREEN,Location.builder().district("Eimsbüttel").build() , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
         ));
     }
 
     public static final List<Post> getStockPostsSortedWithoutPrincipal(){
         return new ArrayList<Post>(List.of(
-                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,"Barmbek" , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
+                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("333", "Janosch","Catching dinner", Instant.parse("2020-11-21T12:00:00.00Z"), Instant.parse("2020-11-21T13:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Barmbek").build() , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("444", "Carsten","Music", Instant.parse("2020-11-22T15:00:00.00Z"), Instant.parse("2020-11-22T17:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
                  ));
     }
 
     public static final List<SendPostDto> getStockSendPostsDtoSortedWithoutPrincipal(){
         return new ArrayList<SendPostDto>(List.of(
-                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("333","Janosch", "Catching dinner", LocalDate.of(2020,11,21),LocalTime.of(13,00), LocalTime.of(14,00),EnumStatus.GREEN,"Barmbek" , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("444", "Carsten","Music", LocalDate.of(2020,11,22),LocalTime.of(16,00), LocalTime.of(18,00),EnumStatus.GREEN,"Sternschanze" , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
+                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("333","Janosch", "Catching dinner", LocalDate.of(2020,11,21),LocalTime.of(13,00), LocalTime.of(14,00),EnumStatus.GREEN,Location.builder().district("Barmbek").build() , EnumStatus.GREEN, EnumCategory.Meal,EnumStatus.BLUE, "I would like to have a dinner out", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("444", "Carsten","Music", LocalDate.of(2020,11,22),LocalTime.of(16,00), LocalTime.of(18,00),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.YELLOW, EnumCategory.Events,EnumStatus.BLUE, "I would like to go to a concert", Instant.parse("2020-11-20T10:56:04Z"))
         ));
     }
 
     public static final List<Post> getStockPostsSortedForPrincipal(){
         return new ArrayList<>(List.of(
-                new Post("111", "Franzi","Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,"Sternschanze" , EnumStatus.BLUE, EnumCategory.Drinks, EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,"Eimsbüttel" , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
+                new Post("111", "Franzi","Drink out", Instant.parse("2020-11-20T09:00:00.00Z"), Instant.parse("2020-11-20T10:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.BLUE, EnumCategory.Drinks, EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new Post("555", "Franzi","Drink out", Instant.parse("2020-11-22T20:00:00.00Z"), Instant.parse("2020-11-22T22:00:00.00Z"),EnumStatus.GREEN,Location.builder().district("Eimsbüttel").build() , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
         ));
     }
 
     public static final List<SendPostDto> getStockSendPostsDtoSortedForPrincipal(){
         return new ArrayList<SendPostDto>(List.of(
-                new SendPostDto("111", "Franzi","Drink out", LocalDate.of(2020,11,20), LocalTime.of(10,00), LocalTime.of(11,00),EnumStatus.GREEN,"Sternschanze" , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
-                new SendPostDto("555", "Franzi","Drink out", LocalDate.of(2020,11,22),LocalTime.of(21,00), LocalTime.of(23,00),EnumStatus.GREEN,"Eimsbüttel" , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
+                new SendPostDto("111", "Franzi","Drink out", LocalDate.of(2020,11,20), LocalTime.of(10,00), LocalTime.of(11,00),EnumStatus.GREEN,Location.builder().district("Sternschanze").build() , EnumStatus.BLUE, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z")),
+                new SendPostDto("555", "Franzi","Drink out", LocalDate.of(2020,11,22),LocalTime.of(21,00), LocalTime.of(23,00),EnumStatus.GREEN,Location.builder().district("Eimsbüttel").build() , EnumStatus.GREEN, EnumCategory.Drinks,EnumStatus.YELLOW, "I would like to have a drink out", Instant.parse("2020-11-20T10:56:04Z"))
         ));
     }
 
     public static final List<Post> getStockFilteredPosts(){
         return new ArrayList<Post>(List.of(
-                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z"))
+                new Post("222", "Rebekka","Getting active", Instant.parse("2020-11-20T09:30:00.00Z"), Instant.parse("2020-11-20T12:30:00.00Z"),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z"))
                 ));
     }
 
     public static final List<SendPostDto> getStockFilteredSendPostsDto(){
         return new ArrayList<SendPostDto>(List.of(
-                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,"Altona" , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z"))
+                new SendPostDto("222", "Rebekka","Getting active", LocalDate.of(2020,11,20),LocalTime.of(10,30), LocalTime.of(13,30),EnumStatus.YELLOW,Location.builder().district("Altona").build() , EnumStatus.YELLOW, EnumCategory.Exercise,EnumStatus.GREEN, "I would like to do some exercise", Instant.parse("2020-11-20T10:56:04Z"))
                 ));
     }
 
