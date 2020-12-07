83c72f0bd06e5a3af4f45d1d45510b2676a1087c
Saskia <busch.saskia09@gmail.com>
2020-12-07 18:44:00 +0100

    minor changes

--- frontend/src/googleMaps/GoogleMapsPosts.js
+++ frontend/src/googleMaps/GoogleMapsPosts.js
@@ -56,6 +56,7 @@ export default function GoogleMapsPosts({ day, indexDay }) {
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    version: "3.42.9",
     libraries,
   });
   if (loadError) return "Error loading maps";
