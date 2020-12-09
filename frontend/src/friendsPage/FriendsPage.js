92335919a335b1fbbaf28f910c8d1722c080008a
Saskia <busch.saskia09@gmail.com>
2020-12-09 18:12:32 +0100

    style add friend

--- frontend/src/friendsPage/FriendsPage.js
+++ frontend/src/friendsPage/FriendsPage.js
@@ -15,13 +15,14 @@ export default function FriendsPage() {
       <Header title="Your friends" />
       <FriendsLayout>
         <form onSubmit={handleSubmit}>
-          <label>
+          <LabelStyled>
             Add friend
             <input
               value={friendUsername}
               onChange={(event) => setFriendUsername(event.target.value)}
+              placeholder="enter username"
             />
-          </label>
+          </LabelStyled>
         </form>
         <FriendsList friends={friends} deleteFriend={deleteFriend} />
       </FriendsLayout>
@@ -39,6 +40,23 @@ export default function FriendsPage() {
 const FriendsLayout = styled.div`
   display: grid;
   grid-template-rows: min-content 1fr;
+  grid-template-columns: 1fr;
   grid-row-gap: var(--size-m);
-  justify-content: center;
+  justify-items: center;
+  padding: var(--size-m) var(size-xxl);
+`;
+
+const LabelStyled = styled.label`
+  display: grid;
+  justify-items: center;
+  font-weight: bold;
+
+  input {
+    margin-top: 1px;
+    text-align: center;
+    width: 200px;
+    height: 25px;
+    border-radius: 5px;
+    border: var(--turquoise-main) solid 1px;
+  }
 `;
