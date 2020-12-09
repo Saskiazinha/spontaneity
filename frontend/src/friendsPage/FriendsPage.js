b52358238bfeaf98e07f3807bd4304d940cbc852
Saskia <busch.saskia09@gmail.com>
2020-12-09 11:19:14 +0100

    rough page layout friends

--- frontend/src/friendsPage/FriendsPage.js
+++ frontend/src/friendsPage/FriendsPage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components/macro";
+import SpontaneityHeader from "../commons/navigation/SpontaneityHeader";
+import Footer from "../commons/navigation/Footer";
+import Header from "../commons/Header";
 
 export default function FriendsPage() {
-  return <div>friends</div>;
+  return (
+    <>
+      <SpontaneityHeader />
+      <Header title="Your friends" />
+      <FriendsLayout>
+        <div>
+          <label>
+            Add friend
+            <input />
+          </label>
+        </div>
+        <div>friends</div>
+      </FriendsLayout>
+      <Footer />
+    </>
+  );
 }
+
+const FriendsLayout = styled.div`
+  display: grid;
+  grid-template-rows: min-content 1fr;
+  grid-row-gap: var(--size-m);
+  justify-content: center;
+`;
