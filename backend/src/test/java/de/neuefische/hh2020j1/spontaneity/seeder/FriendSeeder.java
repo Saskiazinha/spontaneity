c4ba8f7341e4b673c679b8ae774c367233a2a359
Saskia <busch.saskia09@gmail.com>
2020-12-09 10:39:50 +0100

    FriendsControllerTest

--- backend/src/test/java/de/neuefische/hh2020j1/spontaneity/seeder/FriendSeeder.java
+++ backend/src/test/java/de/neuefische/hh2020j1/spontaneity/seeder/FriendSeeder.java
@@ -2,13 +2,14 @@ package de.neuefische.hh2020j1.spontaneity.seeder;
 
 import de.neuefische.hh2020j1.spontaneity.dto.FriendDto;
 import de.neuefische.hh2020j1.spontaneity.model.SpontaneityUser;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FriendSeeder {
 
-    public static final List<FriendDto> getStockFriends(){
+    public static List<FriendDto> getStockFriends(){
         return new ArrayList<>(List.of(
                 new FriendDto("janosch123","Janosch","Nachname"),
                 new FriendDto("eva123", "Eva","Nachname"),
@@ -16,7 +17,7 @@ public class FriendSeeder {
         ));
     }
 
-    public static final List<FriendDto> getStockFriendsWithAdded(){
+    public static List<FriendDto> getStockFriendsWithAdded(){
         return new ArrayList<>(List.of(
                 new FriendDto("janosch123","Janosch","Nachname"),
                 new FriendDto("eva123", "Eva","Nachname"),
@@ -25,11 +26,25 @@ public class FriendSeeder {
         ));
     }
 
-    public static final SpontaneityUser getStockSpontaneityUser(){
-        return new SpontaneityUser("carsten123", "carsten@email.de", "Carsten", "Nachname", "password",getStockFriends());
+    public static SpontaneityUser getStockSpontaneityUser(){
+        String password=new BCryptPasswordEncoder().encode("a-password");
+        return new SpontaneityUser("carsten123", "carsten@email.de", "Carsten", "Nachname", password, getStockFriends());
     }
 
-    public static final FriendDto getStockFriendDto(){
+    public static SpontaneityUser getSecondStockSpontaneityUser(){
+        String password=new BCryptPasswordEncoder().encode("a-password");
+        return new SpontaneityUser("franzi123", "franzi@email.de", "Franzi", "Nachname", password, getStockFriends());
+    }
+
+    public static FriendDto getStockFriendDto(){
         return new FriendDto("carsten123", "Carsten", "Nachname");
+    }
+
+    public static FriendDto getSecondStockFriendDto(){
+        return new FriendDto("franzi123", "Franzi", "Nachname");
+    }
+
+    public static FriendDto getThirdStockFriendDto(){
+        return new FriendDto("rebekka123", "Rebekka", "Nachname");
     }
 }
