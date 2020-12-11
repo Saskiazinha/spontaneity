e468f306bdd38dd80999e8af25d87f0630827540
Saskia <busch.saskia09@gmail.com>
2020-12-11 10:11:07 +0100

    add reload of posts after change of friends

--- frontend/src/hooks/FriendsHook.js
+++ frontend/src/hooks/FriendsHook.js
@@ -1,10 +1,13 @@
 import { useEffect, useState, useContext } from "react";
 import UserContext from "../contexts/UserContext";
 import { addFriend, getFriends, removeFriend } from "../service/FriendsService";
+import { getPosts, getTimeMatchingPosts } from "../service/PostService";
+import PostContext from "../contexts/PostContext";
 
 export default function useFriends() {
   const [friends, setFriends] = useState([]);
   const { token, tokenIsValid } = useContext(UserContext);
+  const { setPostsWithoutSeconds } = useContext(PostContext);
 
   useEffect(() => {
     tokenIsValid() &&
@@ -13,11 +16,26 @@ export default function useFriends() {
         .catch(console.log);
   }, [token, tokenIsValid]);
 
+  useEffect(() => {
+    tokenIsValid() &&
+      getPosts(token)
+        .then((posts) => setPostsWithoutSeconds(posts, "posts"))
+        .catch(console.log);
+  }, [token, tokenIsValid, friends]);
+
+  useEffect(() => {
+    tokenIsValid() &&
+      getTimeMatchingPosts(token)
+        .then((matchingPosts) =>
+          setPostsWithoutSeconds(matchingPosts, "matchingPosts")
+        )
+        .catch(console.log);
+  }, [token, tokenIsValid, friends]);
+
   const addNewFriend = (friendUsername) =>
     addFriend(token, friendUsername).then((newFriend) =>
       setFriends([...friends, newFriend])
     );
-
   const deleteFriend = (friendUsername) =>
     removeFriend(token, friendUsername)
       .then(() =>
