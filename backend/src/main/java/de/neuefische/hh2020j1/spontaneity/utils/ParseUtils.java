cec788e07523433aa55fff6702927f3bc7ddc446
Saskia <busch.saskia09@gmail.com>
2020-12-12 14:07:42 +0100

    clean ParseUtils

--- backend/src/main/java/de/neuefische/hh2020j1/spontaneity/utils/ParseUtils.java
+++ backend/src/main/java/de/neuefische/hh2020j1/spontaneity/utils/ParseUtils.java
@@ -11,30 +11,10 @@ public class ParseUtils {
 
     public static List<SendPostDto> parseToSendPostDtos(List<Post>posts){
 
-       return posts.stream()
-                .map(post->new SendPostDto(
-                        post.getId(),
-                        post.getCreator(),
-                        post.getFirstName(),
-                        post.getTitle(),
-                        LocalDate.ofInstant(post.getStartPoint(), ZoneId.of("Europe/Berlin")),
-                        LocalTime.ofInstant(post.getStartPoint(),ZoneId.of("Europe/Berlin")),
-                        LocalTime.ofInstant(post.getEndPoint(),ZoneId.of("Europe/Berlin")),
-                        post.getStatusTime(),
-                        post.getAddress(),
-                        post.getDistrict(),
-                        post.getLat(),
-                        post.getLng(),
-                        post.getStatusLocation(),
-                        post.getCategory(),
-                        post.getStatusCategory(),
-                        post.getNotes(),
-                        LocalDateTime.ofInstant(post.getTimestamp(),ZoneId.of("Europe/Berlin"))))
-                        .collect(Collectors.toList());
-
-
+        return posts.stream()
+                .map(ParseUtils::parseToSendPostDto)
+                .collect(Collectors.toList());
     }
-
 
     public static SendPostDto parseToSendPostDto(Post post){
         return new SendPostDto(
